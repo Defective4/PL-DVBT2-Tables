0e90570d7c3ebacfdc76a3906ce35013281e0710
Defective <def3ctive4@gmail.com>
2024-07-31 22:01:01 +0200

    Update emitel-downloader/src/main/java/io/github/defective4/dvbtables/emitel/converter/DVBConverter.java

--- emitel-downloader/src/main/java/io/github/defective4/dvbtables/emitel/converter/DVBConverter.java
+++ emitel-downloader/src/main/java/io/github/defective4/dvbtables/emitel/converter/DVBConverter.java
@@ -1,10 +1,19 @@
 package io.github.defective4.dvbtables.emitel.converter;
 
 import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class DVBConverter {
     public static String convertToDVBTable(Map<String, Map<String, Integer>> table) {
         StringBuilder dvbBuilder = new StringBuilder();
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        fmt.setTimeZone(TimeZone.getTimeZone("UTC"));
+        dvbBuilder.append("[date]\n");
+        dvbBuilder.append(fmt.format(new Date()));
+        dvbBuilder.append("\n");
 
         table.entrySet().forEach(transmitterEntry -> {
             dvbBuilder.append(String.format("[dvb-t/pl-%s-dvb-t2]\n", transmitterEntry.getKey()));
