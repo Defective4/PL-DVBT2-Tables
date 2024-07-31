1486afad366b475aeb1db32d47a1b435a3132a2a
Defective4 <def3ctive4@gmail.com>
2024-07-31 21:51:30 +0200

    Update Main.java

--- emitel-downloader/src/main/java/io/github/defective4/dvbtables/emitel/Main.java
+++ emitel-downloader/src/main/java/io/github/defective4/dvbtables/emitel/Main.java
@@ -87,6 +87,7 @@ public class Main {
                 fmt.setTimeZone(TimeZone.getTimeZone("UTC"));
                 writer.println("# Emitel transmitters list  ");
                 writer.println("*Last updated: " + fmt.format(new Date()) + "*  \n");
+                writer.println("> This table is updated every Sunday, at 00:00 UTC");
                 writer.write(markdownTables);
             }
 
